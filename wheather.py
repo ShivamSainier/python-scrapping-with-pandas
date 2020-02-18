0cceeca575ae6d01dfb599d8ad4339c0bb5d2c48
shivam007-ss <ss1907364@gmail.com>
2020-02-18 14:38:56 +0530

    make dataframe

--- wheather.py
+++ wheather.py
@@ -1,13 +1,16 @@
 import requests
 import bs4 as bs
+import pandas as pd
 
 req=requests.get('https://forecast.weather.gov/MapClick.php?x=133&y=65&site=mfr&zmx=&zmy=&map_x=133&map_y=65#.XkuaaSgzbIU')
 soup=bs.BeautifulSoup(req.content,'html.parser')
-item=soup.find(id='seven-day-forecast-list')
+items=soup.find(id='seven-day-forecast-list')
 
-period=item.find_all(class_="period-name")
-short=item.find_all(class_="short-desc")
-temp=item.find_all(class_="temp temp-low")
-print(period)
-print(short)
-print(temp)
+period=[item.find(class_="period-name").get_text() for item in items]
+short=[item.find(class_="short-desc").get_text() for item in items]
+
+weekframe=pd.DataFrame({
+    'period-time':period,
+    'short':short
+    })
+print(weekframe)
